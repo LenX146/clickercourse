6f570641f9e367f066d72bdcd615aeb8aead2163
Ne5terenko <164169626+LenX146@users.noreply.github.com>
2026-05-20 02:08:41 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
  * вставьте свой Project URL (https://xxxx.supabase.co) в поле url ниже.
  */
 window.SLENG_SUPABASE = {
-    url: "hhttps://cwakmkqumyntxwzwmlhp.supabase.co",
+    url: "https://cwakmkqumyntxwzwmlhp.supabase.co",
     // Можно legacy anon (eyJ...) или новый publishable (sb_publishable_...)
     anonKey: "sb_publishable_Ug8cMwfi0q_hM4hzIu4hHA_ZlF8i3Vu",
 };
